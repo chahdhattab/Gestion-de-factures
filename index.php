4defbdfc2bdae6c552c9ea6f68934fba0cdaaf46
chahdhattab <eechahdee74@gmail.com>
2024-07-15 17:22:58 +0100

    commit

--- index.php
+++ index.php
@@ -4,25 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Form</title>
+    <link rel="stylesheet" href="css/style-login.css">
 </head>
 <body>
 
-
-<div class="login-box">
-    <div class="login-header">
-        <header>Login</header>
-    </div>
-    <div class="input-box">
-        <input type="text" class="input-field" name="id" id="id" placeholder="ID" autocomplete="off" required>
-    </div>
-    <div class="input-box">
-        <input type="password" class="input-field" name="password" id="password" placeholder="Mot de passe" autocomplete="off" required>
-    </div>
-    <div class="login-submit">
-        <button class="login-btn">Se connecter</button>
-    </div>
-</div>
-
+    <form action=".php" method="post">
+        <div class="login-box">
+            <div class="login-header">
+                <header>Bienvenue! </header>
+                <div class="label2"> veuillez vous connecter</div>
+            </div>
+            <div class="input-box">
+                <input type="text" class="input-field" name="id" id="id" placeholder="ID" autocomplete="off" required>
+            </div>
+            <div class="input-box">
+                <input type="password" class="input-field" name="password" id="password" placeholder="Mot de passe" autocomplete="off" required><br><br><br>
+            </div>
+            <div class="login-submit">
+                <button type ="submit "class="login-btn" id="submit">Se connecter</button>
+                
+            </div>
+        </div>
+    </form>
 
 
 
