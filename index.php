21347151acff51d89bb81d423cfd8410808de30d
chahdhattab <eechahdee74@gmail.com>
2024-07-15 16:23:12 +0100

    commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+</head>
+<body>
+
+
+<div class="login-box">
+    <div class="login-header">
+        <header>Login</header>
+    </div>
+    <div class="input-box">
+        <input type="text" class="input-field" name="id" id="id" placeholder="ID" autocomplete="off" required>
+    </div>
+    <div class="input-box">
+        <input type="password" class="input-field" name="password" id="password" placeholder="Mot de passe" autocomplete="off" required>
+    </div>
+    <div class="login-submit">
+        <button class="login-btn">Se connecter</button>
+    </div>
+</div>
+
+
+
+
+
+
+</body>
+</html>
