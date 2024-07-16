1722588280e6a9bd2473a8a71896398836e62943
chahdhattab <eechahdee74@gmail.com>
2024-07-16 16:06:48 +0100

    commit

---
+++ dashboard.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/dashboard-style.css">
+    <title>Dashboard</title>
+</head>
+<body>
+    <div class="container">
+        <aside>
+            <div class="top">
+                <div class="logo">
+                    <img src="images/Sqli-logo.webp" alt="Logo" >
+                </div>
+                <div class="close" >
+                    <img src="images/close.png" alt="Logo" style="width:30px;">
+                </div>
+            </div>
+
+            <div class="sidebar">
+                <a href="#">
+                    <img src="images/dashboard.png" alt="Logo" style="width:30px;">
+                    <h3>Dashboard</h3>
+                </a>
+                <a href="#">
+                    <img src="images/loupe.png" alt="Logo" style="width:30px;">
+                    <h3>Recherche</h3>
+                </a>
+                <a href="#">
+                    <img src="images/invoice.png" alt="Logo" style="width:30px;">
+                    <h3>Factures</h3>
+                </a>
+                <a href="#">
+                    <img src="images/customer.png" alt="Logo" style="width:30px;">
+                    <h3>Clients</h3>
+                </a>
+                <a href="#">
+                    <img src="images/money.png" alt="Logo" style="width:30px;">
+                    <h3>Paiements</h3>
+                </a>
+                <a href="#">
+                    <img src="images/logout.png" alt="Logo" style="width:30px;">
+                    <h3>Déconnexion</h3>
+                </a>
+            </div>
+            
+        </aside>
+    </div>
+</body>
+</html>
